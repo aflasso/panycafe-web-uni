0d364f886969bae21b440644ed50cc93309600a1
aflasso <andresfelipe.lasso@gmail.com>
2025-02-15 16:06:32 -0500

    Quitar script s

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-let indice = 0;
-const slides = document.querySelectorAll(".slide");
-
-function moverCarrusel(direccion) {
-    slides[indice].classList.remove("active");
-    indice = (indice + direccion + slides.length) % slides.length;
-    slides[indice].classList.add("active");
-}
